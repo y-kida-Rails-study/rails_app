49af000f828aa9f6ddb2b15048b66fd599640c6e
EC2 Default User <ec2-user@ip-172-31-7-17.ap-northeast-1.compute.internal>
2023-04-04 12:29:17 +0000

    5-1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(title = '')
+    default_title = "RailsApp"
+    if title.blank?
+     default_title
+    else
+      "#{title} | #{default_title}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/hello'
+  get 'static_pages/about' 
+  root 'static_pages#hello'
 end
